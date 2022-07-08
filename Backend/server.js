5bebc02eb0b69b89a9bbc4055dd2e314e559c7fa
ANKIT GUPTA <72732381+Ankit-Gupta18@users.noreply.github.com>
2022-07-08 09:23:43 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -13,6 +13,13 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.options("/*", function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.send(200);
+});
+
 
 app.post('/post', async (req, res) => {
   const { linkValue, quoteValue } = req.body;
