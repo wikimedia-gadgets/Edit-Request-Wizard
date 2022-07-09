2ef4669e85d5279dae7852dc54f2f0109ef535c6
ANKIT GUPTA <72732381+Ankit-Gupta18@users.noreply.github.com>
2022-07-09 18:36:11 +0530

    Addressed mentor reviews in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -7,21 +7,10 @@ const port = parseInt(process.env.PORT, 10);
 
 const app = express()
 app.use(bodyParser.json())
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "test.wikipedia.org"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-app.options("/*", function(req, res, next){
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  res.send(200);
-});
+app.use(cors()) 
 
 
-app.post('/post', async (req, res) => {
+app.post('/api/v1/verifyQuote', async (req, res) => {
   const { linkValue, quoteValue } = req.body;
   try {
     fetch(linkValue)
@@ -36,13 +25,6 @@ app.post('/post', async (req, res) => {
     res.send('failure')
   } 
 })
-
-
-
-
-
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
