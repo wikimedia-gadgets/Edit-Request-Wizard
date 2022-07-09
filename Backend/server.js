345744995f1b9e3780ab01dc1da9f396fa0602fe
ANKIT GUPTA <ankitguptabhabha@gmail.com>
2022-07-09 18:32:51 +0530

    Addressed mentor reviews

--- Backend/server.js
+++ Backend/server.js
@@ -3,19 +3,14 @@ const { JSDOM } = require('jsdom');
 var bodyParser = require('body-parser');
 var cors = require('cors')
 
-const port = 3000
+const port = parseInt(process.env.PORT, 10);
 
 const app = express()
 app.use(bodyParser.json())
 app.use(cors()) 
 
 
-// app.get('/get', (req, res) => {
-//     res.status(200);
-//     res.send('Hello World!')
-// })
-
-app.post('/post', async (req, res) => {
+app.post('/api/v1/verifyQuote', async (req, res) => {
   const { linkValue, quoteValue } = req.body;
   try {
     fetch(linkValue)
@@ -30,13 +25,6 @@ app.post('/post', async (req, res) => {
     res.send('failure')
   } 
 })
-
-
-
-
-
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
