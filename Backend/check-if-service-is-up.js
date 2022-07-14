6913b78ed495d5ec7ac57d35aa87598919259ab7
ANKIT GUPTA <ankitguptabhabha@gmail.com>
2022-07-14 20:01:43 +0530

    Fixed endpoint to toolforge in check-if-service-is-up.js

--- Backend/check-if-service-is-up.js
+++ Backend/check-if-service-is-up.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 const { exec } = require('node:child_process')
 
-fetch('localhost:3000/ping').then(function(response){
+fetch('https://edit-wizard.toolforge.org/ping').then(function(response){
     if(!response.ok){
         exec('-e "Subject: Test message subject\n\nTest message" | /usr/sbin/exim -odf -i tools.edit-wizard@toolforge.org', (err, output) => {})
     }
