a8de419352a0e217ed330ecd90b3ba98a6f98930
ANKIT GUPTA <ankitguptabhabha@gmail.com>
2022-07-16 22:32:47 +0530

    fixed email link command

--- Backend/check-if-service-is-up.js
+++ Backend/check-if-service-is-up.js
@@ -3,6 +3,6 @@ const { exec } = require('node:child_process')
 
 fetch('https://edit-wizard.toolforge.org/ping').then(function(response){
     if(!response.ok){
-        exec('-e "Subject: Test message subject\n\nTest message" | /usr/sbin/exim -odf -i tools.edit-wizard@toolforge.org', (err, output) => {})
+        exec('echo -e "Subject: Test message subject\n\nTest message" | /usr/sbin/exim -odf -i tools.edit-wizard@toolforge.org', (err, output) => {})
     }
 })
