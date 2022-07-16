6923e8eaaa50129b642e2e075c56cd855e08006d
ANKIT GUPTA <ankitguptabhabha@gmail.com>
2022-07-17 00:53:28 +0530

    updated mail content in check-if-service-is-up

--- Backend/check-if-service-is-up.js
+++ Backend/check-if-service-is-up.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 const { exec } = require('child_process')
 
 fetch('https://edit-wizard.toolforge.org/ping').then(function(response){
-    const statusCode = response.statusCode;
+    const statusCode = response.status;
     const statusText = response.statusText;
     if(!response.ok){
         exec(`echo "Edit Request Wizard service is not running. You are requested to look into it as soon as possible.\nStatus Code : ${statusCode} \nStatus Text : ${statusText}" | /usr/bin/mail -s "Edit-Wizard server not running" tools.edit-wizard@toolforge.org`, (err, output) => {})
