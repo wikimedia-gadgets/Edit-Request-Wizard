27b5fb85a862a72a9ea5434165032b86c637c28a
ANKIT GUPTA <ankitguptabhabha@gmail.com>
2022-07-24 09:00:31 +0530

    Added source validation

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { JSDOM } = require('jsdom');
 const fetch = require('node-fetch');
+const got = require('got');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
@@ -15,6 +16,37 @@ app.use(cors());
 app.get('/ping', function(req, res) {
   res.send('pong')
 })
+
+
+//API to verify source
+app.post('/api/v1/verifySource', async (req, res) => {
+  const linkValue = req.body;
+  const url = new URL(linkValue.linkValue);
+  try {
+    got( "https://en.wikipedia.org/w/index.php?title=Wikipedia:TESTING-DONT-USE-unreliable.json&action=raw").json()
+    .then((json) => {
+      (json).forEach(element => {
+        const Origins = element.list;
+        if(typeof Origins == "undefined"){
+          return;
+        }
+        if (Origins.some(origin => url.origin.includes(origin))) {
+          const comment = element.comment;
+          const kind = element.kind;
+          const flag = true;
+          res.send({comment, flag, kind});
+        }
+      });
+    }); 
+  } 
+  catch (error) {
+    console.log(error)
+    res.send('failure')
+    res.sendStatus(404);
+  } 
+})
+
+
 
 // API to verify the quote if it comes from source
 app.post('/api/v1/verifyQuote', async (req, res) => {
