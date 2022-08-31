1d868294938fd707deb77b1a3f150c805c90b02b
ANKIT GUPTA <ankitguptabhabha@gmail.com>
2022-08-31 22:40:36 +0530

    fixed #16

--- Backend/check-if-service-is-up.js
+++ Backend/check-if-service-is-up.js
@@ -1,10 +1,27 @@
 const fetch = require('node-fetch');
-const { exec } = require('child_process')
+// const { exec } = require('child_process')
+const nodemailer = require("nodemailer");
+
+// fetch('https://edit-wizard.toolforge.org/ping').then(function(response){
+//     const statusCode = response.status;
+//     const statusText = response.statusText;
+//     if(!response.ok){
+//         exec(`echo "Edit Request Wizard service is not running. You are requested to look into it as soon as possible.\nStatus Code : ${statusCode} \nStatus Text : ${statusText}" | /usr/bin/mail -s "Edit-Wizard server not running" tools.edit-wizard@toolforge.org`, (err, output) => {})
+//     }
+// })
 
 fetch('https://edit-wizard.toolforge.org/ping').then(function(response){
     const statusCode = response.status;
     const statusText = response.statusText;
     if(!response.ok){
-        exec(`echo "Edit Request Wizard service is not running. You are requested to look into it as soon as possible.\nStatus Code : ${statusCode} \nStatus Text : ${statusText}" | /usr/bin/mail -s "Edit-Wizard server not running" tools.edit-wizard@toolforge.org`, (err, output) => {})
+        let transporter = nodemailer.createTransport({ host: "mail.tools.wmflabs.org" });
+        transporter.sendMail({
+            from: "", // sender address
+            to: "ankitguptabhabha@gmail.com", // list of receivers
+            subject: "Edit Request Wizard service is not running", // Subject line
+            text: `Edit Request Wizard service is not running. You are requested to look into it as soon as possible.\nStatus Code : ${statusCode} \nStatus Text : ${statusText}`, // plain text body
+            // html: "<b>Hello world?</b>", // html body
+          });
     }
 })
+
